8642e68feedca134371744e3d28ad039b022885f
Teodor Hatlenes Blikås <teodorhb@student.uia.no>
2024-05-27 09:52:49 +0000

    all

--- src/27_Pat/include/libc/stddef.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-#define NULL ((void*)0)
-
--- src/27_Pat/include/libc/stdio.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-void putchar(char c);
-void puts(const char* string);
-bool print(const char* data, size_t length);
-void printf(const char* format, ...);
